f47ea338466b75909e1507c98d709e175326b3a1
Santhosh k <72366696+likil33@users.noreply.github.com>
2025-01-22 15:22:59 +0530

    Initial commit

---
+++ ServiceFiles/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  NetworkManagerSDK
+//
+//  Created by Santhosh K on 22/01/25.
+//
+
+import Foundation
+
+public class NetworkManager {
+    public static func getRequestCall(_ url:String) {
+        print("called framework \(url)")
+    }
+}
+
+
